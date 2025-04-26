caa828deeba495a25a22156eda92994457c2b1ca
David Kanekanian <dkanekanian1@gmail.com>
2025-04-26 17:49:45 +0100

    overlay widget fn

--- src/material_ui/_widget.py
+++ src/material_ui/_widget.py
@@ -8,4 +8,17 @@ class Widget(QtWidgets.QWidget):
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.setObjectName(self.__class__.__name__)
+
+    def overlay_widget(self, widget: QtWidgets.QWidget) -> None:
+        """Overlay a widget on top of this widget.
+
+        Ownership will also be set to this widget.
+        """
+        widget.setParent(self)
+        if self.layout() is not None:
+            raise NotImplementedError("Multiple overlay widgets not supported yet")
+        # TODO: add some other kind of 'overlay' layout similar to graphics anchors?
+        layout = QtWidgets.QVBoxLayout(self)
+        layout.setSpacing(0)
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.addWidget(widget)
