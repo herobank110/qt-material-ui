31b2747dbd27523bd858c14a13c1ef65e1952f94
David Kanekanian <dkanekanian1@gmail.com>
2025-04-27 14:52:07 +0100

    style

--- src/material_ui/switch.py
+++ src/material_ui/switch.py
@@ -1,6 +1,9 @@
 from qtpy import QtWidgets
 from material_ui._component import Component, Signal
 
+md_comp_switch_unselected_track_outline_color = "#79747E"
+md_comp_switch_unselected_track_color = "#E6E0E9"
+
 
 class Switch(Component):
     """Switches toggle the selection of an item on or off."""
@@ -13,5 +16,15 @@ class Switch(Component):
         # self.checked = defaultChecked
 
         self.setFixedSize(52, 32)
-
-        self.setStyleSheet("background-color:red;border-radius:16px;")
+        self.setStyleSheet(
+            ";".join(
+                map(
+                    ":".join,
+                    {
+                        "background-color": md_comp_switch_unselected_track_color,
+                        "border-radius": "16px",
+                        "border": f"2px solid {md_comp_switch_unselected_track_outline_color}",
+                    }.items(),
+                )
+            )
+        )
