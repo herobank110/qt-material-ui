3c158f1de07790013813ba6d6b0f31297a37cfdf
David Kanekanian <dkanekanian1@gmail.com>
2025-04-27 19:01:57 +0100

    savea

--- src/material_ui/switch.py
+++ src/material_ui/switch.py
@@ -1,4 +1,4 @@
-from qtpy import QtCore, QtGui
+from qtpy import QtCore, QtGui, QtWidgets
 from material_ui._component import Component, Signal, effect, use_state
 
 md_comp_switch_unselected_track_outline_color = "#79747E"
@@ -20,6 +20,30 @@ class Switch(Component):
 
         self.setFixedSize(52, 32)
 
+        graphics_scene = QtWidgets.QGraphicsScene()
+        graphics_scene.setSceneRect(self.rect())
+        handle = graphics_scene.addEllipse(
+            QtCore.QRect(),
+            QtCore.Qt.NoPen,
+            QtGui.QBrush(md_comp_switch_selected_handle_color),
+        )
+        handle.setRect(QtCore.QRect(0, 0, 16, 16))
+        handle.setPos(QtCore.QPoint(4, (32 - 16) / 2 - 4))
+
+        handle.setRect(QtCore.QRect(0, 0, 28, 28))
+        handle.setPos(QtCore.QPoint(52 - 28 - 2, 2))
+
+        graphics_view = QtWidgets.QGraphicsView()
+        graphics_view.setRenderHint(QtGui.QPainter.Antialiasing)
+        graphics_view.setParent(self)
+        graphics_view.setStyleSheet(
+            "background:transparent;border:none;border-radius:none;"
+        )
+        # graphics_view.move(0, 0)
+        # graphics_view.resize(50, 50)
+        graphics_view.setScene(graphics_scene)
+        graphics_view.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+
     def mousePressEvent(self, event: QtGui.QMouseEvent) -> None:  # noqa: N802
         if event.button() == QtCore.Qt.LeftButton:
             # Toggle the checked state
@@ -29,16 +53,22 @@ class Switch(Component):
     @effect(selected)
     def _apply_style(self) -> None:
         """Apply the style based on the selected state."""
-        style = {
-            "background-color": md_comp_switch_unselected_track_color,
-            "border-radius": "16px",
-            "border": f"2px solid {md_comp_switch_unselected_track_outline_color}",
-        }
-        if self.selected.get():
-            style.update(
-                {
-                    "background-color": md_comp_switch_selected_track_color,
-                    "border": "none",
-                }
-            )
+        if not self.selected.get():
+            # unselected
+            style = {
+                "background-color": md_comp_switch_unselected_track_color,
+                "border": f"2px solid {md_comp_switch_unselected_track_outline_color}",
+            }
+        else:
+            # selected
+            style = {
+                "background-color": md_comp_switch_selected_track_color,
+                "border": "none",
+            }
+
+        style.update(
+            {
+                "border-radius": "16px",
+            }
+        )
         self.sx.set(style)
