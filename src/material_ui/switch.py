e1c218dd5a5e5e41cfb25471f16ee8e390a22345
David Kanekanian <dkanekanian1@gmail.com>
2025-04-28 01:32:42 +0100

    to member variables

--- src/material_ui/switch.py
+++ src/material_ui/switch.py
@@ -11,6 +11,8 @@ state_layer_color = "rgba(0, 0, 0, 40)"
 _STATE_LAYER_MARGIN = 4
 _TRACK_WIDTH = 52
 _TRACK_HEIGHT = 32
+_SWITCH_WIDTH = _TRACK_WIDTH + _STATE_LAYER_MARGIN * 2
+_SWITCH_HEIGHT = _TRACK_HEIGHT + _STATE_LAYER_MARGIN * 2
 _TRACK_OUTLINE_WIDTH = 2
 _HANDLE_UNSELECTED_WIDTH = 16
 _HANDLE_PRESSED_WIDTH = 28
@@ -50,33 +52,31 @@ class Switch(Component):
     def __init__(self) -> None:
         super().__init__()
 
-        self.setFixedSize(52 + 8, 32 + 8)
+        self.setFixedSize(_SWITCH_WIDTH, _SWITCH_HEIGHT)
 
-        state_layer = Shape()
-        state_layer.setParent(self)
-        state_layer.sx.set(
+        self._state_layer = Shape()
+        self._state_layer.setParent(self)
+        self._state_layer.sx.set(
             {
                 "background-color": state_layer_color,
                 "border-radius": "20px",
             }
         )
-        state_layer.setGeometry(QtCore.QRect(52 - (32 - 8) - 8, 0, 32 + 8, 32 + 8))
-        state_layer.setVisible(self.hovered.get())  # initial state - TODO: use binding
+        self._state_layer.setGeometry(QtCore.QRect(52 - (32 - 8) - 8, 0, 32 + 8, 32 + 8))
+        self._state_layer.setVisible(self.hovered.get())  # initial state - TODO: use binding
         # For some reason this fn needs to be wrapped in a lambda.
-        self.hovered.changed.connect(lambda value: state_layer.setVisible(value))
+        self.hovered.changed.connect(lambda value: self._state_layer.setVisible(value))
         # TODO: create a Shape class and have duplicate stuff like visible as Variable.
         # ripple.visible.bind(self.hovered)
 
-        handle = Shape()
-        handle.setParent(self)
-        handle.sx.set(
+        self._handle = Shape()
+        self._handle.setParent(self)
+        self._handle.sx.set(
             {
                 "background-color": md_comp_switch_selected_handle_color,
                 "border-radius": "14px",
             }
         )
-        handle.setGeometry(self._handle_geometry.get())
-        self._handle_geometry.changed.connect(handle.setGeometry)
 
         # Set the internal selected state but use the change_requested
         # signal as source of truth, so using it as a 'controlled' input
