6d95f21584611ba9c7769586f7feb674df6a61b6
David Kanekanian <dkanekanian1@gmail.com>
2025-04-29 22:45:26 +0100

    expose imports

--- src/material_ui/__init__.py
+++ src/material_ui/__init__.py
@@ -1,9 +1,17 @@
 """Common imports for Material UI components."""
 
 from material_ui.button import Button
+from material_ui.shape import Shape
+from material_ui.switch import Switch
 from material_ui.typography import Typography
+from material_ui._component import Component, use_state, effect
 
 __all__ = [
     "Button",
+    "Component",
+    "Shape",
+    "Switch",
     "Typography",
+    "use_state",
+    "effect",
 ]
