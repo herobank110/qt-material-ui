ff4b69c7df338cb616c8b7a16fd1ef4ca312f2ca
David Kanekanian <dkanekanian1@gmail.com>
2025-05-01 08:33:25 +0100

    save

--- src/material_ui/button.py
+++ src/material_ui/button.py
@@ -21,15 +21,15 @@ class Button(Component):
     def __init__(self) -> None:
         super().__init__()
 
-        self.sx.set({
-            "background-color": "red",
-            # "color": "white",
-            "border-radius": "8px",
-            "padding": "8px 16px",
-            "border": "none",
-        })
+        self.sx.set(
+            {
+                "background-color": "red",
+                "border-radius": "8px",
+                "padding": "8px 16px",
+                "border": "none",
+            }
+        )
 
         self._label = Typography()
         self._label.text.bind(self.text)
         self.overlay_widget(self._label)
-
