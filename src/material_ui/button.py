fa7f86f1a9b76af0cbfac9af6e461438790d09ba
David Kanekanian <dkanekanian1@gmail.com>
2025-05-11 16:38:11 +0100

    save

--- src/material_ui/button.py
+++ src/material_ui/button.py
@@ -2,8 +2,9 @@ from typing import Literal, cast
 from material_ui.shape import Shape
 from material_ui.tokens import md_comp_elevated_button as elevated_tokens
 from material_ui._component import Component, use_state
+from material_ui.tokens._utils import resolve_token
 from material_ui.typography import Typography
-from qtpy import QtCore
+from qtpy import QtCore, QtGui, QtWidgets
 
 
 ButtonVariant = Literal[
@@ -26,31 +27,43 @@ class Button(Component):
 
         self.sx.set(
             {
-        #         "background-color": elevated_tokens.container_color,
-        #         "border-radius": "8px",
+                #         "background-color": elevated_tokens.container_color,
+                #         "border-radius": "8px",
                 "padding": "8px 16px",
-        #         "border": "none",
+                #         "border": "none",
             }
         )
-        self.setFixedHeight(40)
+        self.setFixedSize(100, 50)
 
         self._container = Shape()
         self._container.corner_shape.set("full")
-        self._container.sx.set({
-            "background-color": elevated_tokens.container_color,
-            "padding": "8px 16px",
-        })
+        self._container.sx.set(
+            {
+                "background-color": elevated_tokens.container_color,
+                "padding": "8px 16px",
+            }
+        )
         self._container.setParent(self)
-        self._container.resize(self.size())
-        # self._container.resize(80, 40)
-        # self.overlay_widget(self._container)
+        self._container.move(5, 5)
+        self._container.resize(self.size().shrunkBy(QtCore.QMargins(5, 5, 5, 5)))
+        shadow_color = resolve_token(elevated_tokens.container_shadow_color)
+        shadow_color.setAlphaF(0.3)
+        container_drop_shadow = QtWidgets.QGraphicsDropShadowEffect(
+            parent=self._container,
+            blurRadius=resolve_token(elevated_tokens.container_elevation),
+            color=shadow_color,
+            offset=QtCore.QPointF(1, 1),
+        )
+        self._container.setGraphicsEffect(container_drop_shadow)
 
         self._label = Typography()
         self._label.text.bind(self.text)
-        self._label.sx.set({
-            "color": elevated_tokens.label_text_color,
-            "font-size": elevated_tokens.label_text_size,
-            "font-weight": elevated_tokens.label_text_weight,
-        })
+        self._label.sx.set(
+            {
+                "color": elevated_tokens.label_text_color,
+                "font-size": elevated_tokens.label_text_size,
+                "font-weight": elevated_tokens.label_text_weight,
+            }
+        )
         margins = QtCore.QMargins(24, 0, 24, 0)
         self.overlay_widget(self._label, margins)
