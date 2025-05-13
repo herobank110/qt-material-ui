4c4a432c37fa4315e7104d21e75d3117fb3ed62c
David Kanekanian <dkanekanian1@gmail.com>
2025-05-13 22:59:43 +0100

    draw source too!

--- src/material_ui/elevation.py
+++ src/material_ui/elevation.py
@@ -50,23 +50,15 @@ _ELEVATION_AMBIENT_BLUR_RADIUS_MAP: dict[DesignToken, int] = {
 class MyGraphicsEffect(QGraphicsEffect):
     """Testing Effect."""
 
-    def __init__(self) -> None:
-        super().__init__()
-        self._x = 0
-
     def boundingRectFor(self, rect: QRectF) -> QRectF:
         """Override the bounding rectangle to add extra padding."""
         return rect.adjusted(-5, -5, 5, 5)
 
     def draw(self, painter: QPainter) -> None:
         """Override the draw method to add custom drawing."""
-        painter.setBrush(QColor(255, 0, self._x * 80, 50))
-        painter.setClipRect(self.sourceBoundingRect(), Qt.ClipOperation.ReplaceClip)
-        # painter.drawRect(self.boundingRect().adjusted(-1, -1, 1, 1))
-
-    def test(self):
-        self._x = 2
-        self.update()
+        painter.setBrush(QColor(255, 0, 0, 50))
+        painter.drawRect(self.boundingRect().adjusted(-1, -1, 1, 1))
+        self.drawSource(painter)
 
 
 class Elevation(Component):
@@ -79,68 +71,63 @@ class Elevation(Component):
     def __init__(self) -> None:
         super().__init__()
 
-        self.sx.set({"background-color": "black"})
-
         f = MyGraphicsEffect()
         f.setParent(self)
         self.setGraphicsEffect(f)
-        from qtpy.QtCore import QTimer
 
-        QTimer.singleShot(100, lambda: f.test())
+    #     # self.setAttribute(Qt.WidgetAttribute.WA_StyledBackground)
+    #     # self.sx.set({"background-color": "black"})
+    #     # _key_shadow = QGraphicsDropShadowEffect()
+    #     # _key_shadow.setParent(self)
+    #     # _key_shadow.setBlurRadius(10)
+    #     # _key_shadow.setOffset(5, 3)
+    #     # _key_shadow.setColor(QColor("rgba(244,255,255,80)"))
+    #     # self.setGraphicsEffect(_key_shadow)
 
-        # self.setAttribute(Qt.WidgetAttribute.WA_StyledBackground)
-        # self.sx.set({"background-color": "black"})
-        # _key_shadow = QGraphicsDropShadowEffect()
-        # _key_shadow.setParent(self)
-        # _key_shadow.setBlurRadius(10)
-        # _key_shadow.setOffset(5, 3)
-        # _key_shadow.setColor(QColor("rgba(244,255,255,80)"))
-        # self.setGraphicsEffect(_key_shadow)
+    #     self._key_shape = Shape()
+    #     self._key_shape.setParent(self)
+    #     # Needs a background color for the Qt's drop shadow to work.
+    #     self._key_shape.sx.set({"background-color": "red"})
+    #     self._key_shape.corner_shape.bind(self.corner_shape)
+    #     self._key_shape._size.bind(self._size)
+    #     self._key_shadow = QGraphicsDropShadowEffect()
+    #     self._key_shape.setGraphicsEffect(self._key_shadow)
 
-        self._key_shape = Shape()
-        self._key_shape.setParent(self)
-        # Needs a background color for the Qt's drop shadow to work.
-        self._key_shape.sx.set({"background-color": "red"})
-        self._key_shape.corner_shape.bind(self.corner_shape)
-        self._key_shape._size.bind(self._size)
-        self._key_shadow = QGraphicsDropShadowEffect()
-        self._key_shape.setGraphicsEffect(self._key_shadow)
+    #     self._ambient_shape = Shape()
+    #     self._ambient_shape.setParent(self)
+    #     self._ambient_shape.sx.set({"background-color": "green"})
+    #     self._ambient_shape.corner_shape.bind(self.corner_shape)
+    #     self._ambient_shape._size.bind(self._size)
+    #     self._ambient_shadow = QGraphicsDropShadowEffect()
+    #     self._ambient_shadow.setParent(self._ambient_shape)
+    #     self._ambient_shape.setGraphicsEffect(self._ambient_shadow)
 
-        self._ambient_shape = Shape()
-        self._ambient_shape.setParent(self)
-        self._ambient_shape.sx.set({"background-color": "green"})
-        self._ambient_shape.corner_shape.bind(self.corner_shape)
-        self._ambient_shape._size.bind(self._size)
-        self._ambient_shadow = QGraphicsDropShadowEffect()
-        self._ambient_shadow.setParent(self._ambient_shape)
-        self._ambient_shape.setGraphicsEffect(self._ambient_shadow)
+    # @effect(shadow_color)
+    # def _apply_shadow_colors(self):
+    #     """Apply shadow colors."""
+    #     color = resolve_token(self.shadow_color.get())
+    #     if not isinstance(color, QColor):
+    #         raise RuntimeError(
+    #             f"invalid shadow_color token: expected QColor, got {type(color).__name__}"
+    #         )
 
-    @effect(shadow_color)
-    def _apply_shadow_colors(self):
-        """Apply shadow colors."""
-        color = resolve_token(self.shadow_color.get())
-        if not isinstance(color, QColor):
-            raise RuntimeError(
-                f"invalid shadow_color token: expected QColor, got {type(color).__name__}"
-            )
+    #     key_color = QColor(color)
+    #     key_color.setAlphaF(0.3)
+    #     self._key_shadow.setColor(key_color)
 
-        key_color = QColor(color)
-        key_color.setAlphaF(0.3)
-        self._key_shadow.setColor(key_color)
+    #     ambient_color = QColor(color)
+    #     ambient_color.setAlphaF(0.15)
+    #     self._ambient_shadow.setColor(color)
 
-        ambient_color = QColor(color)
-        ambient_color.setAlphaF(0.15)
-        self._ambient_shadow.setColor(color)
+    # @effect(elevation)
+    # def _apply_elevation(self):
+    #     """Apply elevation."""
+    #     elevation = self.elevation.get()
+    #     print(f"elevation: {elevation}")
 
-    @effect(elevation)
-    def _apply_elevation(self):
-        """Apply elevation."""
-        elevation = self.elevation.get()
-        print(f"elevation: {elevation}")
-
-        self._key_shadow.setBlurRadius(_ELEVATION_KEY_BLUR_RADIUS_MAP[elevation])
-        self._key_shadow.setOffset(_ELEVATION_KEY_OFFSET_MAP[elevation])
-        self._ambient_shadow.setBlurRadius(
-            _ELEVATION_AMBIENT_BLUR_RADIUS_MAP[elevation]
-        )
-        self._ambient_shadow.setOffset(_ELEVATION_AMBIENT_OFFSET_MAP[elevation])
+    #     self._key_shadow.setBlurRadius(_ELEVATION_KEY_BLUR_RADIUS_MAP[elevation])
+    #     self._key_shadow.setOffset(_ELEVATION_KEY_OFFSET_MAP[elevation])
+    #     self._ambient_shadow.setBlurRadius(
+    #         _ELEVATION_AMBIENT_BLUR_RADIUS_MAP[elevation]
+    #     )
+    #     self._ambient_shadow.setOffset(_ELEVATION_AMBIENT_OFFSET_MAP[elevation])
