1dbe5232abb77ed496f8a3536c6b02319f5c016d
David Kanekanian <dkanekanian1@gmail.com>
2025-05-17 16:29:08 +0100

    load the font

--- src/material_ui/icon.py
+++ src/material_ui/icon.py
@@ -2,8 +2,10 @@
 
 from typing import cast, Literal
 from material_ui._component import Component, effect, use_state
+from material_ui._font_utils import install_default_fonts
 from material_ui.tokens import md_sys_color
 from qtpy.QtWidgets import QLabel
+from qtpy.QtGui import QFont
 
 # TODO: add all the icons...?
 IconName = Literal[
@@ -16,14 +18,25 @@ IconName = Literal[
 ]
 
 
+IconStyle = Literal[
+    "outlined",
+    "rounded",
+    "sharp",
+]
+
+
 class Icon(Component):
     """Material icon component."""
 
     icon_name = use_state(cast(IconName, "star"))
+    icon_style = use_state(cast(IconStyle, "outlined"))
     color = use_state(md_sys_color.on_surface)
 
     def __init__(self) -> None:
         super().__init__()
+
+        # Ensure fonts are installed (blocking!).
+        install_default_fonts()
 
         self._label = QLabel()
         self.overlay_widget(self._label)
@@ -35,3 +48,14 @@ class Icon(Component):
     @effect(icon_name)
     def _apply_icon_name(self):
         self._label.setText(self.icon_name.get())
+
+    @effect(icon_style)
+    def _apply_font(self):
+        font = QFont(
+            "Material Symbols " + self.icon_style.get().title(),
+            pointSize=24,
+            weight=400,
+        )
+        # font.setVariableAxis(QFont.Tag("FILL"), 1)
+        # font.setVariableAxis(QFont.Tag("GRAD"), 200)
+        self._label.setFont(font)
