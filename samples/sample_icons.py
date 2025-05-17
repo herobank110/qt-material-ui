28fc9e377186be1a22b6e842e6d986c49dc0c04b
David Kanekanian <dkanekanian1@gmail.com>
2025-05-17 16:32:39 +0100

    more rows for rounded styles

--- samples/sample_icons.py
+++ samples/sample_icons.py
@@ -3,32 +3,31 @@
 from qtpy.QtWidgets import QApplication
 from qtpy.QtCore import Qt
 from material_ui.icon import Icon
-from material_ui.layout_basics import Row
+from material_ui.layout_basics import Stack, Row
+from material_ui.tokens import md_sys_color
 
 
 def main() -> None:
     """Main function."""
     app = QApplication()
 
-    window = Row()
+    window = Stack()
     window.alignment = Qt.AlignCenter
     window.gap = 30
-    window.sx = {"background-color": "white"}
+    window.sx = {"background-color": md_sys_color.surface}
     window.resize(700, 200)
 
-    for i, icon_name in enumerate(
-        [
-            "star",
-            "arrow_drop_down",
-            "more_vert",
-            "check",
-            "close",
-            "add",
-        ]
-    ):
-        icon = Icon()
-        icon.icon_name = icon_name
-        window.add_widget(icon)
+    icons = ["star", "arrow_drop_down", "more_vert", "check", "close", "add"]
+
+    for icon_style in ["outlined", "rounded", "sharp"]:
+        row = Row()
+        row.gap = 20
+        window.add_widget(row)
+        for icon_name in icons:
+            icon = Icon()
+            icon.icon_name = icon_name
+            icon.icon_style = icon_style
+            row.add_widget(icon)
 
     window.show()
     app.exec_()
