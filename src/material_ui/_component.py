6972a97dedbef7921b9bc9134ecbddd06917f7ac
David Kanekanian <dkanekanian1@gmail.com>
2025-05-18 11:12:37 +0100

    type ignore comments

--- src/material_ui/_component.py
+++ src/material_ui/_component.py
@@ -6,9 +6,9 @@ from typing_extensions import TypeVarTuple, Unpack
 from qtpy.QtCore import (
     QEvent,
     QObject,
-    Property,
+    Property,  # type: ignore (vscode/pyright error)
     QTimer,
-    Signal as QtSignal,  # type: ignore[import-not-found]
+    Signal as QtSignal,  # type: ignore (vscode/pyright error)
     QMargins,
     QSize,
     Qt,
