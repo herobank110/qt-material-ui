e6ecf947ddbdb20421247e7de63e52b654523216
David Kanekanian <dkanekanian1@gmail.com>
2025-05-18 17:06:54 +0100

    add unit test for effect

--- tests/material_ui/test__component.py
+++ tests/material_ui/test__component.py
@@ -1,26 +1,47 @@
+from pytest_mock import MockerFixture
 from pytestqt.qtbot import QtBot
 
-from material_ui._component import Component, use_state
-
-
-class MyComponent(Component):
-    a = use_state("")
+from material_ui._component import Component, effect, use_state
 
 
 def test_Component_state_bind_on_assignment(qtbot: QtBot):
-    component1 = MyComponent()
-    component1.a = "hello"
-    qtbot.add_widget(component1)
-    component2 = MyComponent()
-    component2.a = "hey"
-    qtbot.add_widget(component2)
-    assert component1.a == "hello"
-    assert component2.a == "hey"
+    class C(Component):
+        a = use_state("")
 
-    component2.a = component1.a
-    assert component1.a == "hello"
-    assert component2.a == "hello"
+    c1 = C()
+    c1.a = "hello"
+    qtbot.add_widget(c1)
+    c2 = C()
+    c2.a = "hey"
+    qtbot.add_widget(c2)
+    assert c1.a == "hello"
+    assert c2.a == "hey"
 
-    component1.a = "hi"
-    assert component1.a == "hi"
-    assert component2.a == "hi"
+    c2.a = c1.a
+    assert c1.a == "hello"
+    assert c2.a == "hello"
+
+    c1.a = "hi"
+    assert c1.a == "hi"
+    assert c2.a == "hi"
+
+
+def test_Component_effect_is_called_on_deps(qtbot: QtBot, mocker: MockerFixture):
+    stub = mocker.stub()
+
+    class C(Component):
+        a = use_state("hello")
+
+        @effect(a)
+        def a_effect(self):
+            stub(self.a)
+
+    c = C()
+    qtbot.add_widget(c)
+
+    assert stub.call_count == 1
+    assert stub.call_args == mocker.call("hello")
+
+    c.a = "hi"
+    assert stub.call_count == 2
+    assert stub.call_args == mocker.call("hi")
