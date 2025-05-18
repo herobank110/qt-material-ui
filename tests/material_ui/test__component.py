a5e2276c6b78ccdf92f1983e454a4e82b406f702
David Kanekanian <dkanekanian1@gmail.com>
2025-05-18 16:21:02 +0100

    change values for easier testing

--- tests/material_ui/test__component.py
+++ tests/material_ui/test__component.py
@@ -28,12 +28,18 @@ class MyComponent(Component):
 
 def test_Component_state_bind_on_assignment(qtbot: QtBot):
     component1 = MyComponent()
+    component1.a = "hello"
     qtbot.add_widget(component1)
     component2 = MyComponent()
+    component2.a = "hey"
     qtbot.add_widget(component2)
+    assert component1.a == "hello"
+    assert component2.a == "hey"
+
     component2.a = component1.a
     assert component1.a == "hello"
     assert component2.a == "hello"
+
     component1.a = "hi"
     assert component1.a == "hi"
     assert component2.a == "hi"
