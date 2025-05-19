a0b54b89caabbc5a2164cc93cc68576a3ebe6c21
David Kanekanian <dkanekanian1@gmail.com>
2025-05-20 00:13:14 +0100

    refactor constants

--- tests/material_ui/test__component_State.py
+++ tests/material_ui/test__component_State.py
@@ -5,16 +5,20 @@ from material_ui._component import State, _TransitionConfig
 
 
 def test_State_transition_applies_over_time(qtbot: QtBot):
-    my_state = State(10.0, "my_state")
-    my_state.set_transition(_TransitionConfig(50, QEasingCurve.Type.Linear))
-    my_state.set_value(20.0)
+    duration_ms = 50
+    start_value = 10.0
+    end_value = 20.0
+
+    state = State(start_value, "state")
+    state.set_transition(_TransitionConfig(duration_ms, QEasingCurve.Type.Linear))
+    state.set_value(end_value)
 
     periodically_inspected_values: list[float] = []
-    for _ in range(50 // 10 + 2):
-        periodically_inspected_values.append(my_state.get_value())
-        qtbot.wait(10)
+    for _ in range(duration_ms // 5 + 1):
+        periodically_inspected_values.append(state.get_value())
+        qtbot.wait(duration_ms // 5)
 
-    assert my_state.get_value() == 20.0
+    assert state.get_value() == end_value
     assert periodically_inspected_values == sorted(periodically_inspected_values)
-    assert min(periodically_inspected_values) >= 10.0
-    assert max(periodically_inspected_values) <= 20.0
+    assert min(periodically_inspected_values) >= start_value
+    assert max(periodically_inspected_values) <= end_value
