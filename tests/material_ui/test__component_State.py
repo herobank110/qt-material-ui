7f43236a9821f3805fd07a37df9562e5c136fabe
David Kanekanian <dkanekanian1@gmail.com>
2025-05-20 00:36:59 +0100

    test animation too

--- tests/material_ui/test__component_State.py
+++ tests/material_ui/test__component_State.py
@@ -24,12 +24,22 @@ def test_State_transition_applies_over_time(qtbot: QtBot):
     assert max(periodically_inspected_values) <= end_value
 
 
-def test_State_transition_instant_changes_applies_last_value(
+def test_State_transition_instant_changes_take_last_value(
     qtbot: QtBot,
 ):
     state = State(10.0, "state")
     state.set_transition(_TransitionConfig(50, QEasingCurve.Type.Linear))
     state.set_value(20.0)
     state.set_value(10.0)
+    qtbot.wait(60)
+    assert state.get_value() == 10.0
+
+
+def test_State_animate_to_instant_changes_take_last_value(
+    qtbot: QtBot,
+):
+    state = State(10.0, "state")
+    state.animate_to(20.0, 50, QEasingCurve.Type.Linear)
+    state.animate_to(10.0, 50, QEasingCurve.Type.Linear)
     qtbot.wait(60)
     assert state.get_value() == 10.0
