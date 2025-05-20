d6cf52144876fb4e4aecbf9d8b420ad820f6d167
David Kanekanian <dkanekanian1@gmail.com>
2025-05-20 23:32:51 +0100

    refactoring

--- src/material_ui/progress_indicators/circular_progress.py
+++ src/material_ui/progress_indicators/circular_progress.py
@@ -2,13 +2,12 @@
 
 from typing import cast
 
-from qtpy.QtCore import QMargins, QSize
+from qtpy.QtCore import QMargins, QRect, QSize
 from qtpy.QtGui import QPainter, QPaintEvent, QPen
 from qtpy.QtWidgets import QSizePolicy
 
 from material_ui._component import Component, effect, use_state
 from material_ui.tokens import md_comp_circular_progress_indicator as tokens
-from material_ui.tokens import md_comp_progress_indicator as base_tokens
 from material_ui.tokens._utils import resolve_token
 
 
@@ -50,20 +49,24 @@ class CircularProgress(Component):
     def paintEvent(self, event: QPaintEvent) -> None:  # noqa: N802
         """Paint the circular progress indicator."""
         super().paintEvent(event)
+
         painter = QPainter(self)
         painter.setRenderHint(QPainter.RenderHint.Antialiasing)
 
-        track_thickness = cast("int", resolve_token(base_tokens.track_thickness))
-        track_color = resolve_token(base_tokens.track_color)
-        # Subtract padding so the line isn't drawn half out of bounds.
-        p = track_thickness // 2
-        draw_rect = self.rect().marginsRemoved(QMargins(p, p, p, p))
-
-        painter.setPen(QPen(track_color, float(track_thickness)))
-        painter.drawEllipse(draw_rect)
-
-        active_color = resolve_token(tokens.active_indicator_color)
-        painter.setPen(QPen(active_color, float(track_thickness)))
-        painter.drawArc(draw_rect, self._start_angle, self._span_angle)
+        color = resolve_token(tokens.active_indicator_color)
+        painter.setPen(QPen(color, float(self._thickness)))
+        painter.drawArc(self._arc_rect, self._start_angle, self._span_angle)
 
         painter.end()
+
+    @property
+    def _thickness(self) -> int:
+        """Returns the thickness of the line."""
+        return cast("int", resolve_token(tokens.active_indicator_width))
+
+    @property
+    def _arc_rect(self) -> QRect:
+        """Returns the rectangle for the arc."""
+        # Subtract padding so the line isn't drawn half out of bounds.
+        p = self._thickness // 2
+        return self.rect().marginsRemoved(QMargins(p, p, p, p))
