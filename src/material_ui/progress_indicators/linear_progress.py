78ae81c47c0838208842d5861e20cf7f3cbfe1df
David Kanekanian <dkanekanian1@gmail.com>
2025-05-22 00:21:42 +0100

    refactoring

--- src/material_ui/progress_indicators/linear_progress.py
+++ src/material_ui/progress_indicators/linear_progress.py
@@ -5,13 +5,20 @@ from typing import cast
 
 from qtpy.QtCore import QEasingCurve, QPointF, QRect, Qt, QVariantAnimation
 from qtpy.QtGui import QColor, QPainter, QPaintEvent
-from qtpy.QtWidgets import QSizePolicy, QWidget
+from qtpy.QtWidgets import QSizePolicy
 
-from material_ui._component import Component, effect, use_state
+from material_ui._component import effect, use_state
 from material_ui.progress_indicators._base_progress import BaseProgress
-from material_ui.shape import Shape
 from material_ui.tokens import md_comp_linear_progress_indicator as tokens
 from material_ui.tokens._utils import resolve_token
+
+_INDETERMINATE_ANIM_DURATION = 2000
+_INDETERMINATE_ANIM_EASING = QEasingCurve(QEasingCurve.Type.BezierSpline)
+_INDETERMINATE_ANIM_EASING.addCubicBezierSegment(
+    QPointF(0.2, 0.3),
+    QPointF(0.9, 1.0),
+    QPointF(1.0, 1.0),
+)
 
 
 class LinearProgress(BaseProgress):
@@ -36,14 +43,6 @@ class LinearProgress(BaseProgress):
             self._bar1_rect = QRect(0, 0, self.value * self.width(), self.height())
             self._bar2_rect = QRect()
         else:
-            duration = 2000
-            easing = QEasingCurve(QEasingCurve.Type.BezierSpline)
-            easing.addCubicBezierSegment(
-                QPointF(0.2, 0.3),
-                QPointF(0.9, 1.0),
-                QPointF(1.0, 1.0),
-            )
-
             # Magic numbers from material-web implementation:
             # https://github.com/material-components/material-web/blob/main/progress/internal/_linear-progress.scss#L141
             anim = QVariantAnimation()
@@ -52,8 +51,8 @@ class LinearProgress(BaseProgress):
             anim.setKeyValueAt(0.2, 0.0 - 1.45167)
             anim.setKeyValueAt(0.5915, 0.836714 - 1.45167)
             anim.setEndValue(2.00611 - 1.45167)
-            anim.setDuration(duration)
-            anim.setEasingCurve(easing)
+            anim.setDuration(_INDETERMINATE_ANIM_DURATION)
+            anim.setEasingCurve(_INDETERMINATE_ANIM_EASING)
             anim.setLoopCount(-1)
             anim.valueChanged.connect(partial(setattr, self, "_bar1_translate"))
             anim.start()
@@ -65,8 +64,8 @@ class LinearProgress(BaseProgress):
             anim.setKeyValueAt(0.6915, 0.561479)
             anim.setKeyValueAt(0.93, 0.08)
             anim.setEndValue(0.08)
-            anim.setDuration(duration)
-            anim.setEasingCurve(easing)
+            anim.setDuration(_INDETERMINATE_ANIM_DURATION)
+            anim.setEasingCurve(_INDETERMINATE_ANIM_EASING)
             anim.setLoopCount(-1)
             anim.valueChanged.connect(partial(setattr, self, "_bar1_scale"))
             anim.start()
@@ -77,8 +76,8 @@ class LinearProgress(BaseProgress):
             anim.setKeyValueAt(0.25, 0.376519 - 0.548889)
             anim.setKeyValueAt(0.4835, 0.843862 - 0.548889)
             anim.setEndValue(1.60278 - 0.548889)
-            anim.setDuration(duration)
-            anim.setEasingCurve(easing)
+            anim.setDuration(_INDETERMINATE_ANIM_DURATION)
+            anim.setEasingCurve(_INDETERMINATE_ANIM_EASING)
             anim.setLoopCount(-1)
             anim.valueChanged.connect(partial(setattr, self, "_bar2_translate"))
             anim.start()
@@ -89,8 +88,8 @@ class LinearProgress(BaseProgress):
             anim.setKeyValueAt(0.1915, 0.457104)
             anim.setKeyValueAt(0.4415, 0.72796)
             anim.setEndValue(0.08)
-            anim.setDuration(duration)
-            anim.setEasingCurve(easing)
+            anim.setDuration(_INDETERMINATE_ANIM_DURATION)
+            anim.setEasingCurve(_INDETERMINATE_ANIM_EASING)
             anim.setLoopCount(-1)
             anim.valueChanged.connect(partial(setattr, self, "_bar2_scale"))
             anim.start()
