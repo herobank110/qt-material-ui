b8c8701ffc7c91fd4ae08709e7e99fef8b8a5ac6
David Kanekanian <dkanekanian1@gmail.com>
2025-05-25 16:10:42 +0100

    save

--- samples/music_player/app.py
+++ samples/music_player/app.py
@@ -1,35 +1,56 @@
 """Main app component."""
 
-import asyncio
 from pathlib import Path
 
-import httpx
 from material_ui._component import Component, effect, use_state
 from material_ui.buttons.filled_button import FilledButton
 from material_ui.layout_basics import Stack
 from material_ui.tokens import md_sys_color
 from material_ui.typography import Typography
-from PySide6 import QtAsyncio
-from qtpy.QtCore import Qt, QUrl
-from qtpy.QtMultimedia import QMediaDevices
+from qtpy.QtCore import QPointF, Qt, QUrl
+from qtpy.QtGui import QImage, QPainter, QPixmap, QRadialGradient
+from qtpy.QtMultimedia import QAudioOutput, QMediaPlayer
 from qtpy.QtWidgets import QLabel
+
+songs = [
+    "https://www.freecol.org/images/fearless-sailors.ogg",
+    "https://www.freecol.org/images/founders.ogg",
+    "https://www.freecol.org/images/settlers-routine.ogg",
+    "https://www.freecol.org/images/sunrise.ogg",
+    "https://www.freecol.org/images/tailwind.ogg",
+    "https://www.freecol.org/images/musicbox.ogg",
+    "https://www.freecol.org/images/FreeCol-opening.ogg",
+    "https://www.freecol.org/images/FreeCol-menu.ogg",
+]
 
 
 class MusicPlayerApp(Component):
     def __init__(self) -> None:
         super().__init__()
-        self.setMinimumSize(200, 400)
+        self.setMinimumSize(300, 400)
         self.sx = {"background-color": md_sys_color.background}
 
         stack = Stack(alignment=Qt.AlignmentFlag.AlignCenter)
 
         title = Typography()
+        title.alignment = Qt.AlignmentFlag.AlignCenter
         title.variant = "headline-medium"
         title.text = "Music Player"
         stack.add_widget(title)
 
         image_label = QLabel()
+        image_label.setAttribute(Qt.WidgetAttribute.WA_StyledBackground)
         image_label.setFixedSize(200, 200)
+        image = QImage(200, 200, QImage.Format.Format_ARGB32_Premultiplied)
+        painter = QPainter(image)
+        painter.fillRect(image.rect(), "transparent")
+        grad = QRadialGradient(QPointF(100, 100), 100)
+        grad.setColorAt(0.0, "white")
+        grad.setColorAt(1.0, "black")
+        painter.setBrush(grad)
+        painter.drawEllipse(QPointF(100, 100), 100, 100)
+        painter.end()
+        image_label.setPixmap(QPixmap(image))
         stack.add_widget(image_label)
 
         play_button = FilledButton()
@@ -38,13 +59,6 @@ class MusicPlayerApp(Component):
 
         self.overlay_widget(stack)
 
-        audio_files = asyncio.run(download_songs())
-        # print(audio_files)
-        # from audioplayer import AudioPlayer
-        # player = AudioPlayer(r"C:\Users\davk1\Downloads\song18.mp3")
-        # player.play()
-        from qtpy.QtMultimedia import QAudioOutput, QMediaPlayer
-
         media_player = QMediaPlayer()
         media_player.setParent(self)
         audio_output = QAudioOutput()
@@ -52,36 +66,8 @@ class MusicPlayerApp(Component):
         print(audio_output.device().description())
         media_player.setAudioOutput(audio_output)
         media_player.setSource(QUrl("https://www.freecol.org/images/FreeCol-menu.ogg"))
-        media_player.hasAudioChanged.connect(lambda: print("hasAudioChanged", media_player.hasAudio()))
+        media_player.hasAudioChanged.connect(
+            lambda: print("hasAudioChanged", media_player.hasAudio())
+        )
         audio_output.setVolume(100.0)
         media_player.play()
-
-    # @effect(image_url)
-    # def _load_image(self) -> None:
-    #     if self.image_url:
-    #         future = asyncio.ensure_future(fetch(self.image_url))
-    #         future.add_done_callback(lambda _: print("done!"))
-
-
-async def download_songs() -> list[Path]:
-    download_path = Path(__file__).parent / "downloaded"
-    if not download_path.exists():
-        print("Downloading songs...")
-        download_path.mkdir(parents=True, exist_ok=True)
-        async with httpx.AsyncClient() as client:
-            responses = await asyncio.gather(
-                client.get("https://www.freecol.org/images/fearless-sailors.ogg"),
-                client.get("https://www.freecol.org/images/founders.ogg"),
-                client.get("https://www.freecol.org/images/settlers-routine.ogg"),
-                client.get("https://www.freecol.org/images/sunrise.ogg"),
-                client.get("https://www.freecol.org/images/tailwind.ogg"),
-                client.get("https://www.freecol.org/images/musicbox.ogg"),
-                client.get("https://www.freecol.org/images/FreeCol-opening.ogg"),
-                client.get("https://www.freecol.org/images/FreeCol-menu.ogg"),
-            )
-        for response in responses:
-            file_name = Path(str(response.request.url)).name
-            file_path = download_path / file_name
-            with file_path.open("wb") as fp:
-                fp.write(response.content)
-    return list(download_path.glob("*.ogg"))
