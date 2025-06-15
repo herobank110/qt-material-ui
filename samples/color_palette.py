175711e5524695fc79450894437d69e85f2b319f
David Kanekanian <dkanekanian1@gmail.com>
2025-06-15 15:17:37 +0100

    color hex textfield

--- samples/color_palette.py
+++ samples/color_palette.py
@@ -5,6 +5,7 @@ from dataclasses import dataclass, replace
 from material_ui._component import Component, effect, use_state
 from material_ui.layout_basics import Row, Stack
 from material_ui.switch import Switch
+from material_ui.text_fields.outlined_text_field import OutlinedTextField
 from material_ui.tokens import md_sys_color
 from material_ui.typography import Typography
 from qtpy.QtCore import QMargins, Qt
@@ -40,6 +41,11 @@ class Controls(Component):
         title.text = "Color Palette"
         stack.add_widget(title)
 
+        self._color_hex_textfield = OutlinedTextField()
+        self._color_hex_textfield.label = "Color (Hex)"
+        self._color_hex_textfield.on_change.connect(self._on_change_color_hex)
+        stack.add_widget(self._color_hex_textfield)
+
         dark_mode_row = Row()
         dark_mode_row.gap = 5
         dark_mode_label = Typography()
@@ -57,9 +63,13 @@ class Controls(Component):
     @effect(state)
     def _apply_state(self) -> None:
         self._dark_mode_switch.selected = self.state.is_dark
+        self._color_hex_textfield.value = self.state.color_hex
 
     def _on_change_dark_mode(self, selected: bool) -> None:  # noqa: FBT001
         self.state = replace(self.state, is_dark=selected)
+
+    def _on_change_color_hex(self, value: str) -> None:
+        self.state = replace(self.state, color_hex=value)
 
 
 class DemoColorPalette(Component):
