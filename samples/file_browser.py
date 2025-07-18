06b55a3a6bc0e79d0c1d995b4dec0055a977cb1c
David Kanekanian <dkanekanian1@gmail.com>
2025-07-18 09:47:15 +0100

    basic directory listing

--- samples/file_browser.py
+++ samples/file_browser.py
@@ -1,9 +1,13 @@
 """File browser demo app."""
 
+from functools import reduce
 from pathlib import Path
+from typing import Any
 
-from material_ui._component import Component, use_state
+from material_ui._component import Component, Signal, effect, use_state
+from material_ui.buttons.text_button import TextButton
 from material_ui.layout_basics import Stack
+from material_ui.tokens import md_sys_color
 from qtpy.QtCore import QMargins, Qt
 from qtpy.QtWidgets import QApplication
 
@@ -36,17 +40,82 @@ MOCK_FILE_SYSTEM = {
 }
 
 
+class DirectoryItem(Component):
+    """A component representing a directory item in the file browser."""
+
+    label = use_state("hi")
+    on_click: Signal
+
+    def __init__(self) -> None:
+        super().__init__()
+        self._button = TextButton()
+        self._button.clicked.connect(self.on_click)
+        self.overlay_widget(self._button)
+
+    @effect(label)
+    def _apply_label(self) -> None:
+        self._button.text = self.label
+
+
 class FileBrowserApp(Component):
     current_path = use_state(Path())
 
     def __init__(self) -> None:
         super().__init__()
 
+        self.resize(300, 400)
+        self.sx = {"background-color": md_sys_color.background}
+
         self.stack = Stack(
             alignment=Qt.AlignmentFlag.AlignTop,
             margins=QMargins(20, 20, 20, 20),
             gap=10,
         )
+        self.overlay_widget(self.stack)
+
+    @effect(current_path)
+    def _apply_directory_listing(self) -> None:
+        # Delete previous items.
+        prev_children = self.stack.findChildren(
+            DirectoryItem,
+            options=Qt.FindChildOption.FindDirectChildrenOnly,
+        )
+        for child in prev_children:
+            child.setParent(None)
+        # Create new items.
+        for path in listdir(self.current_path):
+            item = DirectoryItem()
+            item.label = path.name
+            item.on_click = lambda p=path: self.set_state("current_path", p)
+            self.stack.add_widget(item)
+
+
+def listdir(path: Path) -> list[Path]:
+    """List directory contents as concatenated paths."""
+    current: dict[str, Any] = MOCK_FILE_SYSTEM
+    for part in path.parts:
+        if (maybe_next := current[part]) is not FILE:
+            current = maybe_next
+    return [path / x for x in current]
+
+
+def isfile(path: Path) -> bool:
+    """Check if the path is a file."""
+    return reduce(lambda x, y: x[y], path.parts, MOCK_FILE_SYSTEM) is FILE
+
+
+def test_file_system() -> None:
+    assert listdir(Path("Pictures")) == [
+        Path("Pictures/Vacation"),
+        Path("Pictures/Birthday"),
+        Path("Pictures/Wedding"),
+    ]
+    assert isfile(Path("Pictures/Vacation/Beach"))
+    assert not isfile(Path("Pictures/Vacation"))
+    assert listdir(Path("Documents/Projects/Project 1")) == [
+        Path("Documents/Projects/Project 1/Report"),
+        Path("Documents/Projects/Project 1/Presentation"),
+    ]
 
 
 def main() -> None:
@@ -58,3 +127,4 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
+    # test_file_system()
