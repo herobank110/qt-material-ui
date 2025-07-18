c03bf27f0eb3e2daa6958a7cafd9a02e97f20bb1
David Kanekanian <dkanekanian1@gmail.com>
2025-07-18 15:57:02 +0100

    launch sampler

--- samples/sample_combobox.py
+++ samples/sample_combobox.py
@@ -1,17 +1,25 @@
 """Sample of using the ComboBox."""
 
+from material_ui._component import Component
 from material_ui.combobox import ComboBox
+from material_ui.tokens import md_sys_color
+from qtpy.QtCore import QMargins
 from qtpy.QtWidgets import QApplication
 
 
-class SampleComboBox(ComboBox):
-    def __init__(self):
+class SampleComboBox(Component):
+    def __init__(self) -> None:
         super().__init__()
-        self.resize(300, 100)
-        # Add more ComboBox setup here as needed
+        self.sx = {"background-color": md_sys_color.background}
+
+        cb = ComboBox()
+        cb.items = ["Option 1", "Option 2", "Option 3"]
+        cb.label = "Label"
+
+        self.overlay_widget(cb, QMargins(40, 40, 40, 40))
 
 
-def main():
+def main() -> None:
     app = QApplication([])
     window = SampleComboBox()
     window.show()
