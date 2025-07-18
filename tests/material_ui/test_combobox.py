b82651b37c083376e46c197c0eebb743735e7869
David Kanekanian <dkanekanian1@gmail.com>
2025-07-18 16:28:23 +0100

    unit test

--- tests/material_ui/test_combobox.py
+++ tests/material_ui/test_combobox.py
@@ -1,5 +1,10 @@
-# Empty unit test for ComboBox
+from pytestqt.qtbot import QtBot
+
+from material_ui.combobox import ComboBox
 
 
-def test_combobox_placeholder():
-    assert True
+def test_ComboBox_basic_api(qtbot: QtBot):
+    combo = ComboBox()
+    combo.items = ["Option 1", "Option 2", "Option 3"]
+    combo.label = "Select an option"
+    qtbot.addWidget(combo)
